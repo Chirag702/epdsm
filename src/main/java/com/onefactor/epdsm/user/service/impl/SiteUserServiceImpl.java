d1b1bbce7b9a81e71b1bcd0550711e6f9f3e01c6
Chirag Agarwal <chiragagrawal629@gmail.com>
2024-11-05 23:16:20 +0530

    changes

--- src/main/java/com/onefactor/epdsm/user/service/impl/SiteUserServiceImpl.java
+++ src/main/java/com/onefactor/epdsm/user/service/impl/SiteUserServiceImpl.java
@@ -6,44 +6,47 @@ import com.onefactor.epdsm.user.service.SiteUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class SiteUserServiceImpl implements SiteUserService {
 
-    @Autowired
-    private SiteUserRepository siteUserRepository;
+	@Autowired
+	private SiteUserRepository siteUserRepository;
 
-    @Override
-    public SiteUser save(SiteUser user) {
-        // You might want to handle password hashing here before saving
-        user.setPassword(null); // Ignore password when saving
-        return siteUserRepository.save(user);
-    }
+	@Override
+	public SiteUser save(SiteUser user) {
+		Timestamp currentTimestamp = new Timestamp(System.currentTimeMillis());
+		user.setJoinDate(currentTimestamp);
+		// You might want to handle password hashing here before saving
+		user.setPassword(null); // Ignore password when saving
+		return siteUserRepository.save(user);
+	}
 
-    @Override
-    public Optional<SiteUser> findById(Integer id) {
-        return siteUserRepository.findById(id);
-    }
+	@Override
+	public Optional<SiteUser> findById(Integer id) {
+		return siteUserRepository.findById(id);
+	}
 
-    @Override
-    public List<SiteUser> findAll() {
-        return siteUserRepository.findAll();
-    }
+	@Override
+	public List<SiteUser> findAll() {
+		return siteUserRepository.findAll();
+	}
 
-    @Override
-    public void deleteById(Integer id) {
-        siteUserRepository.deleteById(id);
-    }
+	@Override
+	public void deleteById(Integer id) {
+		siteUserRepository.deleteById(id);
+	}
 
-    @Override
-    public Optional<SiteUser> findByUsername(String username) {
-        return Optional.ofNullable(siteUserRepository.findByUsername(username));
-    }
+	@Override
+	public Optional<SiteUser> findByUsername(String username) {
+		return Optional.ofNullable(siteUserRepository.findByUsername(username));
+	}
 
-    @Override
-    public Optional<SiteUser> findByEmail(String email) {
-        return Optional.ofNullable(siteUserRepository.findByEmail(email));
-    }
+	@Override
+	public Optional<SiteUser> findByEmail(String email) {
+		return Optional.ofNullable(siteUserRepository.findByEmail(email));
+	}
 }
