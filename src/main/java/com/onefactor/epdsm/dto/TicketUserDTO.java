4ac0eaf6683a9d99ecbb0a0cdf060f2486ff07d5
Chirag Agarwal <chiragagrawal629@gmail.com>
2024-11-05 09:15:18 +0530

    changes

--- src/main/java/com/onefactor/epdsm/dto/TicketUserDTO.java
+++ src/main/java/com/onefactor/epdsm/dto/TicketUserDTO.java
@@ -7,19 +7,38 @@ import lombok.Data;
 
 @Data
 public class TicketUserDTO {
-	   
-    private Integer ticketId;
-    private String ticketTrackId;
-    private String title;
-    private String ticketBody;
-    private String status;
-    private String priority;
-    private Timestamp openedTime;
-    
-    // User fields
-    private Integer userId;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String phone;
+
+	private Integer ticketId;
+	private String ticketTrackId;
+	private String title;
+	private String ticketBody;
+	private String status;
+	private String priority;
+	private Timestamp openedTime;
+
+	// User fields
+	private Integer userId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phone;
+	public TicketUserDTO(Integer ticketId, String ticketTrackId, String title, String ticketBody, String status,
+			String priority, Timestamp openedTime, Integer userId, String firstName, String lastName, String email,
+			String phone) {
+		super();
+		this.ticketId = ticketId;
+		this.ticketTrackId = ticketTrackId;
+		this.title = title;
+		this.ticketBody = ticketBody;
+		this.status = status;
+		this.priority = priority;
+		this.openedTime = openedTime;
+		this.userId = userId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phone = phone;
+	}
+
+
 }
--- src/main/java/com/onefactor/epdsm/tickets/repository/TicketRepository.java
+++ src/main/java/com/onefactor/epdsm/tickets/repository/TicketRepository.java
@@ -3,15 +3,17 @@ package com.onefactor.epdsm.tickets.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.onefactor.epdsm.dto.TicketUserDTO;
 import com.onefactor.epdsm.tickets.entity.Ticket;
-
+@Repository
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
 
-	@Query("SELECT new com.onefactor.epdsm.dto.TicketUserDTO("
-			+ "t.id, t.ticketTrackId, t.title, t.ticketBody, t.status, t.priority, t.openedTime, "
-			+ "u.id, u.firstName, u.lastName, u.email, u.phone) "
-			+ "FROM Ticket t JOIN SiteUser u ON t.ticketUser = u.id " + "WHERE t.id = :ticketId")
+	@Query("SELECT new com.onefactor.epdsm.dto.TicketUserDTO(" 
+	         + "t.id, t.ticketTrackId, t.title, t.ticketBody, t.status, t.priority, t.openedTime, "
+	         + "u.id, u.firstName, u.lastName, u.email, u.phone) "
+	         + "FROM Ticket t JOIN SiteUser u ON CAST(t.ticketUser AS int) = u.id "
+	         + "WHERE t.id = :ticketId")
 	TicketUserDTO getTicketById(@Param("ticketId") long ticketId);
 }
